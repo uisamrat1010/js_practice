ed93c767938b10e85dc638b2dc73908d0c3ee530
uisamrat1010 <uisamratc@gmail.com>
2024-10-07 15:53:11 +0530

    first commit

---
+++ reverseString.js
@@ -0,0 +1,10 @@
+const myArr = "tarmas levart ecapS";
+const newArr = myArr.split("").reverse().join("");
+const newArr2 = newArr.split(" ").reverse().join(" ");
+
+//const newARR2 = newArr.split("").reverse().join("");
+console.log(newArr);
+console.log(newArr2);
+
+// o/p   "Space travel samrat"
+// o/p   "Space travel samrat"
